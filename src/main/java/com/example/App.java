5f57f0945552b8af4bac9c2601ef146027eb13bb
Ulrich T <ulrichfokoutakoukam@gmail.com>
2023-09-14 02:25:44 -0400

    show date

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.util.Date;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Ulrich, today is: "  + new Date());
     }
 }
